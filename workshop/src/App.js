a46dd2558fe2bced28bc1421c67fa8575fdbf1b0
Leonardo Andriolo <159090875+LeonardoAndriolo@users.noreply.github.com>
2024-03-12 18:17:13 -0300

    Atualiza o terceiro código

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -1,8 +1,29 @@
+import React from 'react';
+import Input from './components/Input';
+import Button from './components/Button';
+
+
 function App() {
   return (
-    <div className="border-2 border-red-500">
-      <h1 className="text-bold text-blue-300">Hello, World!</h1>
-      <p className="text-underlined">Olá!</p>
+    <div className="flex min-h-screen w-full h-full items-center justify-center">
+      
+      <div className="flex flex-col bg-gray-600 w-2/6 justify-center items-center">
+        <p>
+          Receba sua Proof-Of-Attendance NFT
+        </p>
+
+        {/* <p className="">
+          Receba sua Proof-Of-Attendance NFT
+        </p> */}
+
+        <form className="flex flex-col mt-8">
+          <Input description="Seu nome" label="Nome" placeholder="Bill"/>
+          <Input description="E-mail" label="E-mail para receber news" placeholder="user@user.com"/>
+          <Input description="Wallet" label="Carteira NFT" placeholder="00.00...0000"/>
+        </form>
+        <Button type="Submit" name="Submit" />
+      </div>
+
     </div>
   );
 }
